a3b5422415372ddc6f6ec4b784c29c2f9779f46a
Sifan Guo <todguo86@gmail.com>
2018-03-06 04:20:08 +0800

    fix a bug

--- pat-b-practice/No1006.cpp
+++ pat-b-practice/No1006.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <string>
 
 #define DEBUG
@@ -10,36 +9,45 @@ int main(){
   cin >> inputNum;
   //as indicated by the question,0 < input < 1000
   unsigned inputNums[3];//inputNums[0] is hundred, inputNums[1] is ten.
-  for(unsigned i = 0; i != inputNum.size(); i++){
-    inputNums[i] = inputNum[i] - 48;//change the char into unsigned nums
-    #ifndef DEBUG
-    cout << inputNums[i] << endl;
-    #endif
-  }
-  //one single digit[1,9]
-  if(inputNums[0] == 0 && inputNums[1] == 0 ){
+
+  //numbers in [100,999]
+  if(inputNum.size() == 3){
+    for(unsigned i = 0; i != inputNum.size(); i++){
+      inputNums[i] = inputNum[i] - 48;//change the char into unsigned nums
+      #ifndef DEBUG
+      cout << inputNums[i] << endl;
+      #endif
+    }
+    for(unsigned i = 1; i != inputNums[0] + 1; i++)
+      cout << 'B';
+    for(unsigned i = 1; i != inputNums[1] + 1; i++)
+      cout << 'S';
     for(unsigned i = 1; i != inputNums[2] + 1; i++)
       cout << i;
   }
-  else{
-    //numbers in [10,99]
-    if(inputNums[0] == 0 && inputNums[1] != 0){
-      for(unsigned i = 1; i != inputNums[1] + 1; i++)
-        cout << 'S';
-      for(unsigned i = 1; i != inputNums[2] +1; i++)
-        cout << i;
-    }
-    else{
-      //numbers in [100,999]
-      if(inputNums[0] != 0){
-        for(unsigned i = 1; i != inputNums[0] + 1; i++)
-          cout << 'B';
-        for(unsigned i = 1; i != inputNums[1] + 1; i++)
-          cout << 'S';
-        for(unsigned i = 1; i != inputNums[2] + 1; i++)
-          cout << i;
-      }
-    }
+
+  //numbers in [10,99]
+  if(inputNum.size() == 2){
+      inputNums[1] = inputNum[0] - 48;//change the char into unsigned nums
+      inputNums[2] = inputNum[1] - 48;//change the char into unsigned nums
+      #ifndef DEBUG
+      cout << inputNums[i] << endl;
+      #endif
+    for(unsigned i = 1; i != inputNums[1] + 1; i++)
+      cout << 'S';
+    for(unsigned i = 1; i != inputNums[2] + 1; i++)
+      cout << i;
   }
+  //numbers in [1,9]
+  if(inputNum.size() == 1){
+      inputNums[2] = inputNum[0] - 48;//change the char into unsigned nums
+      #ifndef DEBUG
+      cout << inputNums[i] << endl;
+      #endif
+    for(unsigned i = 1; i != inputNums[2] + 1; i++)
+      cout << i;
+  }
+
 return 0;
 }
+
