e93f40f4e13692fd08ba993df48c6fe5959e8f1e
Sifan Guo <todguo86@gmail.com>
2018-03-05 14:35:55 +0800

    Correcting the question

--- pat-b-practice/No1005.cpp
+++ pat-b-practice/No1005.cpp
@@ -9,12 +9,17 @@ using namespace std;
 
 int main(){
   set <unsigned> exclude;//store the numbers that had been calculated.
-  unsigned trainNum, testNum;
+  unsigned testNum, inputNum;
   vector <int> testNums;
-  cin >> trainNum;
+  cin >> inputNum;
   getchar();//receives the enter(\n)
   
   //exclude.insert(trainNum);
+  unsigned inputFlag = 0;
+while(inputFlag != inputNum){
+    cin >> testNum;
+    if(exclude.find(testNum) != exclude.end()) continue;//ignore the value
+    testNums.push_back(testNum);
   while(trainNum !=1){
     if(trainNum % 2 ==0){
       trainNum = trainNum / 2;
@@ -25,16 +30,12 @@ int main(){
       exclude.insert(trainNum);
     }
   }
+}
   #ifndef DEBUG
   for ( auto &w : exclude)
     cout << w; 
   #endif
   
-  while (getchar() != '\n'){
-    cin >> testNum;
-    testNums.push_back(testNum);
-  }
-
   for ( auto &n : testNums) {
     if (exclude.find(n) != exclude.end())
       testNums.erase(remove(testNums.begin(),testNums.end(),n),testNums.end());
