834d0a62c9e9134ec3b09aa3f1acaddc4881487b
Sifan Guo <todguo86@gmail.com>
2018-03-06 03:53:14 +0800

    fix a bug

--- pat-b-practice/No1006.cpp
+++ pat-b-practice/No1006.cpp
@@ -17,26 +17,29 @@ int main(){
     #endif
   }
   //one single digit[1,9]
-  if(inputNums[0]== 0 && inputNums[1] == 0 ){
+  if(inputNums[0] == 0 && inputNums[1] == 0 ){
     for(unsigned i = 1; i != inputNums[2] + 1; i++)
       cout << i;
   }
-  //numbers in [10,99]
-  if(inputNums[0] == 0 && inputNums[1] != 0){
-    for(unsigned i = 1; i != inputNums[1] + 1; i++)
-      cout << 'S';
-    for(unsigned i = 1; i != inputNums[2] +1; i++)
-      cout << i;
-  }
-
-  //numbers in [100,999]
-  if(inputNums[2] != 0){
-    for(unsigned i = 1; i != inputNums[0] + 1; i++)
-      cout << 'B';
-    for(unsigned i = 1; i != inputNums[1] + 1; i++)
-      cout << 'S';
-    for(unsigned i = 1; i != inputNums[2] + 1; i++)
-      cout << i;
+  else{
+    //numbers in [10,99]
+    if(inputNums[0] == 0 && inputNums[1] != 0){
+      for(unsigned i = 1; i != inputNums[1] + 1; i++)
+        cout << 'S';
+      for(unsigned i = 1; i != inputNums[2] +1; i++)
+        cout << i;
+    }
+    else{
+      //numbers in [100,999]
+      if(inputNums[0] != 0){
+        for(unsigned i = 1; i != inputNums[0] + 1; i++)
+          cout << 'B';
+        for(unsigned i = 1; i != inputNums[1] + 1; i++)
+          cout << 'S';
+        for(unsigned i = 1; i != inputNums[2] + 1; i++)
+          cout << i;
+      }
+    }
   }
 return 0;
 }
